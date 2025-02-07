2f26323202530feb73b33449733b565a6bab45f4
Chris <oscar.sletvold@gmail.com>
2025-02-07 14:57:19 +0100

    First push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Vue_phoneApp',
+  webDir: 'dist'
+};
+
+export default config;
